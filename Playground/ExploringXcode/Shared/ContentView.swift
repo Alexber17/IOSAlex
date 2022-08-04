be10406c48b227fd076662d7ab537fc2104a9f4c
Alexander Bermudez <alexanderbermudez@Alexanders-MacBook-Air.local>
2022-08-04 15:16:34 -0400

    adding test code

---
+++ Playground/ExploringXcode/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Alexander Bermudez on 8/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Playground/ExploringXcode/Shared/ExploringXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploringXcodeApp.swift
+//  Shared
+//
+//  Created by Alexander Bermudez on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ExploringXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
