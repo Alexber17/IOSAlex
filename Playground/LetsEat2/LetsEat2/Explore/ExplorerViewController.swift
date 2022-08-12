ae0ab33b3008e06e7ce4a50d701ff272ad0fc11d
Alexander Bermudez <alexanderbermudez@Alexanders-MacBook-Air.local>
2022-08-12 14:28:59 -0400

    Adding Cristina File

--- Playground/LetsEat2/LetsEat2/Explore/ExplorerViewController.swift
+++ Playground/LetsEat2/LetsEat2/Explore/ExplorerViewController.swift
@@ -7,8 +7,29 @@
 
 import UIKit
 
-class ExplorerViewController: UIViewController {
-
+class ExplorerViewController: UIViewController,UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionsView: UICollectionView, viewForSupplementaryElementOfKind kind :String, at indexPath: IndexPath) -> UICollectionReusableView{
+            let headerView =
+        CollectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        return headerView
+    }
+    
+    
+    @IBAction func unwwindLocationCancel (segue: UIStoryboardSegue){
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell",
+        for: indexPath)
+        return cell
+    }
+    
+    @IBOutlet var CollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +37,5 @@ class ExplorerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
